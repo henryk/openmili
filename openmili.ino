0002a39014b4c2fe38cce62a76fee5fb4cc965b4
Henryk Plötz <henryk@ploetzli.ch>
2015-06-02 20:41:21 +0200

    Implement full transmitter/receiver in the arduino sketch

--- openmili.ino
+++ openmili.ino
@@ -18,28 +18,122 @@ void setup()
   Serial.begin(115200);
   printf_begin();
   delay(1000);
-  Serial.println("OpenMiLight receiver starting");
+  Serial.println("# OpenMiLight Receiver/Transmitter starting");
   mlr.begin();
 }
 
 
+static int dupesPrinted = 0;
+static bool receiving = false;
+static bool escaped = false;
+static uint8_t outgoingPacket[7];
+static uint8_t outgoingPacketPos = 0;
+static uint8_t nibble;
+static enum {
+  IDLE,
+  HAVE_NIBBLE,
+  COMPLETE,
+} state;
+
 void loop()
 {
-  static int dupesPrinted = 0;
-  if (mlr.available()) {
-    printf("\n");
-    uint8_t packet[7];
-    size_t packet_length = sizeof(packet);
-    mlr.read(packet, packet_length);
+  if (receiving) {
+    if (mlr.available()) {
+      printf("\n");
+      uint8_t packet[7];
+      size_t packet_length = sizeof(packet);
+      mlr.read(packet, packet_length);
 
-    for (int i = 0; i < packet_length; i++) {
-      printf("%02X ", packet[i]);
+      for (int i = 0; i < packet_length; i++) {
+        printf("%02X ", packet[i]);
+      }
+    }
+
+    int dupesReceived = mlr.dupesReceived();
+    for (; dupesPrinted < dupesReceived; dupesPrinted++) {
+      printf(".");
     }
   }
-  
-  int dupesReceived = mlr.dupesReceived();
-  for(;dupesPrinted < dupesReceived; dupesPrinted++) {
-    printf(".");
+
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    uint8_t val = 0;
+    bool have_val = true;
+
+    if (inChar >= '0' && inChar <= '9') {
+      val = inChar - '0';
+    } else if (inChar >= 'a' && inChar <= 'f') {
+      val = inChar - 'a' + 10;
+    } else if (inChar >= 'A' && inChar <= 'F') {
+      val = inChar - 'A' + 10;
+    } else {
+      have_val = false;
+    }
+
+    if (!escaped) {
+      if (have_val) {
+        switch (state) {
+          case IDLE:
+            nibble = val;
+            state = HAVE_NIBBLE;
+            break;
+          case HAVE_NIBBLE:
+            if (outgoingPacketPos < sizeof(outgoingPacket)) {
+              outgoingPacket[outgoingPacketPos++] = (nibble << 4) | (val);
+            } else {
+              Serial.println("# Error: outgoing packet buffer full/packet too long");
+            }
+            if (outgoingPacketPos >= sizeof(outgoingPacket)) {
+              state = COMPLETE;
+            } else {
+              state = IDLE;
+            }
+            break;
+          case COMPLETE:
+            Serial.println("# Error: outgoing packet complete. Press enter to send.");
+            break;
+        }
+      } else {
+        switch (inChar) {
+          case ' ':
+          case '\n':
+          case '\r':
+            if (state == COMPLETE) {
+              mlr.write(outgoingPacket, sizeof(outgoingPacket));
+            }
+            if(inChar != ' ') {
+              outgoingPacketPos = 0;
+              state = IDLE;
+            }
+            break;
+          case '.':
+            mlr.resend();
+            delay(1);
+            break;
+          case 'x':
+            Serial.println("# Escaped to extended commands: r - Toggle receiver; Press enter to return to normal mode.");
+            escaped = true;
+            break;
+        }
+      }
+    } else {
+      switch (inChar) {
+        case '\n':
+        case '\r':
+          outgoingPacketPos = 0;
+          state = IDLE;
+          escaped = false;
+          break;
+        case 'r':
+          receiving = !receiving;
+          if (receiving) {
+            Serial.println("# Now receiving");
+          } else {
+            Serial.println("# Now not receiving");
+          }
+          break;
+      }
+    }
   }
 }
 
